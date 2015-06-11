f0c2a922132588349af6b7a52e037e4a75cb8d28
Anita <asovay@gmail.com>
2015-06-10 23:52:48 -0700

    more styling

--- lib/task_actions.rb
+++ lib/task_actions.rb
@@ -3,6 +3,7 @@ require 'sqlite3'
 module TaskList
   class Actions < Database	
 
+    # adds a task record to existing database 'tasks.db'
   	def new_task(new_task, new_desc, new_comp_date)
       statement = "INSERT INTO tasks(task, desc, comp_date) 
                   VALUES(\"#{new_task}\", \"#{new_desc}\", \"#{new_comp_date}\");"
